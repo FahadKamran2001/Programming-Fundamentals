7c85958767ee29dfe0eeaed4643304ea2ea68ee2
FahadKamran2001 <122481348+FahadKamran2001@users.noreply.github.com>
2023-01-12 00:21:15 +0500

    Add files via upload

---
+++ Problem-2_DrawArrow.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+	{
+
+	cout<<"PROBLEM 2\n";
+	cout<<"        /\\               \n";
+	cout<<"       //\\\\             \n";
+	cout<<"      ///\\\\\\           \n";
+	cout<<"     ////\\\\\\\\         \n";	
+	cout<<"    /////\\\\\\\\\\       \n";
+	cout<<"   //////\\\\\\\\\\\\     \n";
+	cout<<"  ///////\\\\\\\\\\\\\\   \n";
+	cout<<" ////////\\\\\\\\\\\\\\\\ \n";
+	cout<<"        ||        \n";
+	cout<<"        ||        \n";
+	cout<<"        ||        \n";
+	cout<<"        ||        \n";
+	cout<<"        ||        \n";
+	cout<<"        ||        \n";
+}
