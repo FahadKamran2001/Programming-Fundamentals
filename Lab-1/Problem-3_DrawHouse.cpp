6b85e5bb9f55f58d6fb2566205996b91d2059a9e
Fahad Kamran <122481348+FahadKamran2001@users.noreply.github.com>
2023-01-12 00:57:20 +0500

    Add files via upload

---
+++ Lab-1/Problem-3_DrawHouse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+	{
+	
+	cout<<"PROBLEM 3\n";
+	cout<<setw(56)<<"        ______________________________                  \n";
+	cout<<setw(56)<<"       /\\                             \\                 \n";
+	cout<<setw(56)<<"      /  \\                             \\                \n";
+	cout<<setw(56)<<"     /    \\         ______              \\               \n";
+	cout<<setw(56)<<"   /      \\	    \\__\\__\\              \\                \n";
+	cout<<setw(56)<<"   /        \\        \\__\\__\\              \\               \n";
+	cout<<setw(56)<<"  /          \\                             \\             \n";
+	cout<<setw(56)<<" /____________\\_____________________________\\            \n";
+	cout<<setw(56)<<" |            |                             |           \n";
+	cout<<setw(56)<<" |            |                             |           \n";
+	cout<<setw(56)<<" |    _______ |         _______             |           \n";
+	cout<<setw(56)<<" |    |     | |         |__|__|             |           \n";
+	cout<<setw(56)<<" |    |     | |         |__|__|             |           \n";
+	cout<<setw(56)<<" |    |     | |                             |           \n";
+	cout<<setw(56)<<" |    |    +| |                             |           \n";
+	cout<<setw(56)<<" |    |     | |                             |           \n";
+	cout<<setw(56)<<" |    |_____| |                             |           \n";
+	cout<<setw(56)<<" |____________|_____________________________|           \n";
+} 
