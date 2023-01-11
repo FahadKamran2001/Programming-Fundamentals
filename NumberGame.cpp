9cfb01aeec7de44afb13e43398f65d87ab7c21b7
FahadKamran2001 <122481348+FahadKamran2001@users.noreply.github.com>
2023-01-12 00:30:15 +0500

    Add files via upload

---
+++ NumberGame.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int num1;
+cout<<"Enter a number"<<endl;
+cin>>num1;
+cout<<"Double it : "<<(num1*2)<<endl;
+cout<<"Add 10 to it : "<<(num1*2+10)<<endl;
+cout<<"half it : "<<((num1*2+10)/2)<<endl;
+cout<<"subtract your number from it : "<<(((num1*2+10)/2)-num1)<<endl;
+cout<<"you get the answer five"<<endl;
+
+}
